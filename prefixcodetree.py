de107b74148840e76c9f6816d4e6e953787885bc
Tuan Pham <ngoctuan.uet.99@gmail.com>
2020-10-07 19:03:33 +0700

    update

---
+++ prefixcodetree.py
@@ -0,0 +1,29 @@
+class prefixcodetree:
+    def init(self):
+        self.tree = [0]*1000
+
+def insert(self,codeword,symbol):
+    index=0
+    for element in codeword: 
+        if element == 1: 
+            index = index * 2 + 2 
+        elif element == 0: 
+            index = index * 2 + 1 
+            self.tree[index]=symbol 
+            
+def decode(self,encodedData, datalen):
+    result="" 
+    index=0 
+    data=""
+    for byte in encodedData:
+        data += f'{byte:0>8b}' 
+    for i in range(datalen): 
+        char = data[i] 
+        if char == '1': 
+            index = index * 2 + 2 
+        elif char == '0': 
+            index = index * 2 + 1 
+    if self.tree[index] != 0: 
+        result += " " + self.tree[index]
+        index=0
+    return result
